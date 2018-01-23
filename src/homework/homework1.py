b03c3d6dda69de642a45597ec47fb3a5190035bd
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-01-22 18:43:49 -0600

    Update homework1.py

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,6 +1,8 @@
 def get_hours_since_midnight(seconds):
-   return seconds // 3600
-def get_minutes(seconds): 
-  return (seconds % 3600) * 60 
-def get_seconds(seconds): 
-   return (seconds % 3600) % 60
+    return (seconds//3600)
+
+def get_minutes(seconds):
+    return (seconds - (get_hours_since_midnight(seconds)*3600))//60
+
+def get_seconds(seconds):
+    return seconds - ((get_hours_since_midnight(seconds)*3600) + (get_minutes(seconds)*60))

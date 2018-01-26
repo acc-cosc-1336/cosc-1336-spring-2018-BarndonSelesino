1388a08dc3d1b7deef21d87dd77d269582fed5f4
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-01-25 21:48:52 -0600

    Delete main_assignment2.py

--- main_assignment2.py
+++
@@ -1,4 +0,0 @@
-from assignment2 import faculty_evaluation_result
-
-'''Write code to call the faculty_evaluation_result function with data of your choice'''
-faculty_evaluation_result(10,10,10,6,14,15):

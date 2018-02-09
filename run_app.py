848d62ce3d73df1dfef9c7bf8828812896791cc3
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-02-08 22:17:14 -0600

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,1 +1,4 @@
-import src.assignment.assignment4
+import src.assignment.main.assignment4
+
+#running this file will run the code in main_homework2.py file without the need to modify
+#code in that file

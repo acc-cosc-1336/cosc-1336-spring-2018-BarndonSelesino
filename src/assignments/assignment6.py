4a094cf8ad22d9b9fe3068ed13a176d512586ecf
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-02-22 21:35:41 -0600

    Add files via upload

--- src/assignments/assignment6.py
+++ src/assignments/assignment6.py
@@ -6,3 +6,25 @@ def get_count_A_C_G_and_T_in_string(dna_string):
     :return: the count of As, Cs, Gs, and Ts in the dna_string
     '''
 
+  
+    count_A = 0
+    count_C = 0
+    count_G = 0
+    count_T = 0
+
+    for ch in dna_string:
+        if ch == 'A' or ch == 'a':
+            count_A += 1
+        elif ch == 'C' or ch == 'c':
+            count_C += 1
+        elif ch == 'G' or ch == 'g':
+            count_G += 1
+        elif ch == 'T' or ch == 't':
+            count_T += 1
+
+    return count_A,count_C,count_G,count_T
+    print ('The letter A apears  ', count_A, 'times. ')
+    print ('The letter C appears ', count_C, 'times. ')
+    print ('The letter G appears ', count_G, 'times. ')
+    print ('The letter T appears ', count_T, 'times. ')
+
--- src/assignments/main_assignment6.py
+++ src/assignments/main_assignment6.py
@@ -13,3 +13,13 @@ A 20, C 12, G 17, T 21
 
 Call the main function in Python Shell or in this file.
 '''
+from src.assignments.assignment6 import get_count_A_C_G_and_T_in_string
+def main():
+    valcount_A,valcount_C,valcount_G,valcount_T = get_count_A_C_G_and_T_in_string('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
+    print('DNA string')
+    print('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
+    print('A ', str(valcount_A),'  C  ', str(valcount_C), '  G  ' , str(valcount_G), '  T  ', str(valcount_T))
+   
+
+
+main()

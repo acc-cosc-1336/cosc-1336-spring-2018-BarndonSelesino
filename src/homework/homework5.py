14388dc9e0936ec79ed8f5ee0baefc4ac28811b5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-02-25 17:18:48 -0600

    Homework 5 help
    write sales data function correction.
    read sales data doesn't return a value.

--- src/homework/homework5.py
+++ src/homework/homework5.py
@@ -3,18 +3,11 @@
 
 #Create another function named read_sales_data with file_object as a parameter.
 #The function will read the file line by line and display to screen to produce the table described in homework 5.
-def write_sales_data(file_object,item,price):
-    #cut out file_object in writesalesdata because it was causing errors
-    infile = open('sales_records.txt', 'w')
-    item = ''
-    price = ''
-    infile.write(item + '\t' + price)
-    infile.close()
+def write_sales_data(file_object, item, price):
 
+    file_object.write(item + '\t' + price + '\n')
 
+def read_sales_data(file_object):
 
-def read_sales_data(file_object):    
-    infile = open('sales_records.txt', 'r')
-    file_contents = infile.read()
-    infile.close()
-    return(file_contents)
+    for line in file_object:
+        print(line.rstrip('\n'))

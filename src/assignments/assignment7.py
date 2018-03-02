55cd6ec3a6e225e9a7db15d4ae6bdbb0d6c0788f
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-03-01 22:07:11 -0600

    Add files via upload

--- src/assignments/assignment7.py
+++ src/assignments/assignment7.py
@@ -8,3 +8,30 @@ sue 8 22 17 14 32 17 24 21 2 9 11 17
 grace 12 28 21 45 26 10
 john 14 32 25 16 89
 '''
+#joe_list = [10, 15, 20, 30, 40]
+#bill_list = [23, 16, 19, 22]
+#sue_list = [8, 22, 17, 14, 32, 17, 24, 21, 2, 9, 11, 17]
+#grace_list = [12, 28, 21, 45, 26, 10]
+#john_list = [14, 32, 25, 16, 89]
+#total = 0
+
+def sum_list_values(value_list):
+    total = 0
+    index = 1
+    while index < len (value_list):
+        total += value_list[index]
+    return(total)
+  
+
+
+
+
+#sum_list_values(list)
+#print(sum(num))
+#total = 0
+    #for value in (joe_list, bill_list, sue_list, grace_list, john_list):
+        #total += value
+    #print(total)
+
+
+
--- src/assignments/main_assignment7.py
+++ src/assignments/main_assignment7.py
@@ -1,5 +1,5 @@
 #write the import for function for assignment7 sum_list_values
-
+from assignment7 import sum_list_values
 '''
 Create a function named process_list that calls the sum_list_values function.
 Prints the list values and the sum of the element in the list as follows:
@@ -19,4 +19,25 @@ sue 8 22 17 14 32 17 24 21 2 9 11 17
 grace 12 28 21 45 26 10
 john 14 32 25 16 89
 
+also pass to process list
 '''
+def process_list(value_list):
+    sum_values = sum_list_value(value_list)
+    print(value, 'sum: ', sum_values)
+
+
+def main():
+    continue_loop == 'Y'
+    while continue_loop == 'Y':
+        value_list = {}
+        value_list.append(input('name:       '))
+        num_values = int(input('Please enter the number of values:      '))
+        for i in range (0, num_values+1):
+            value = int(input('Enter a value:           '))
+            value_list.append(value)
+    process_list(value_list)
+    continue_list = input(' Do you want to do another list? Press Y if so')                    
+        
+        
+        
+    

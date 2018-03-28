bbec098027215f2bf858f8161764461e8a7687c6
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-03-28 17:31:57 -0500

    Add files via upload

---
+++ src/homework/main_homework8.py
@@ -0,0 +1,26 @@
+from homework8 import add_inventory, remove_inventory_widget
+import pickle
+'''
+Write a main function to create an empty dictionary and
+a user-controlled loop to prompt for a widget name and quantity.
+Add the values to the dictionary as key(widget name) and value(quantity) pairs.
+
+After user decides to exit write data to file .
+
+'''
+def main():
+    widgets = {}
+    data = 'y'
+    while data == 'y' or data == 'Y':
+        widget_name = input('Enter a name:  ')
+        quantity = int(input('enter a value:   '))
+        widget_names_and_quantity = add_inventory(widget_name, quantity, widgets)
+        data = input('Continue Y/N?:      ')
+    else:
+        file_object = open('widgets_dictionary.dat', 'wb')
+        picke.dump(widgets, file_object)
+        file_object.close()
+main()
+        
+    
+

34e91ababe00ddadce838f81c36f07d8449ade2c
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-04-03 20:11:58 -0500

    Update main(10).py

--- src/homework/main(10).py
+++ src/homework/main(10).py
@@ -1,3 +1,5 @@
 #write import statements for Player and Die class
-
+from player import
+from die import
 #Create an instance of the Main class and call/execute the roll_doubles method
+class Main():

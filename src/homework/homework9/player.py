ea21eb7edf447d0d0a61cafcbd3fdc7089f0d9a8
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-04-04 20:41:57 -0500

    Add files via upload

--- src/homework/homework9/player.py
+++ src/homework/homework9/player.py
@@ -1,5 +1,5 @@
 #write import statement for Die class
-from die import die, roll
+from die import Die
 '''
 Create a Player class.
 
@@ -11,13 +11,22 @@ class Player:
         '''
         Constructor method creates two Die attributes die1 and die2
         '''
-
+        self.die1 = Die()
+        self.die2 = Die()
 
     def roll_doubles(self):
         '''
         The roll_doubles method that will roll die1 and die2 (attributes from constructor method),
         display rolled values,and continue iterating until a double is rolled.
         '''
-
+        dice1 = 1
+        dice2 = 2
+        while dice1 != dice2:
+            dice1 = self.die1.roll()
+            print('The roll of dice 1 is:    ', dice1)
+            dice2 = self.die2.roll()
+            print('The roll of dice 2 is:    ', dice2)
+        else:
+            print('You have rolled doubles and both dice have die(d)')
 
 

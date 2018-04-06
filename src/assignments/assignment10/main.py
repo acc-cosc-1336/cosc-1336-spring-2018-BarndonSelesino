18008e5ccd390765bca120a7e582d72a56d1a317
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-04-05 21:56:38 -0500

    Add files via upload

---
+++ src/assignments/assignment10/main.py
@@ -0,0 +1,54 @@
+#Write import statements for classes invoice and invoice_item and PRODUCT
+from invoice import  Invoice
+from invoice_item import  InvoiceItem
+from product import Product
+from customer import Customer
+import unittest
+#ASSIGNMENT10 Write import statements for classes product and customer
+
+
+'''
+LOOK AT THE TEST CASES FOR HINTS
+Create an invoice object 
+
+In the loop:
+Create a user controlled loop to continue until y is not typed, in loop...
+    Prompt user for description, quantity, and cost.
+    Create a new InvoiceItem, use the newly created product as a parameter argument 
+    Add values to the InvoiceItem.
+    Add the InvoiceItem to the invoice object.
+    Once user types a letter other than y display the Invoice to screen
+'''
+#ASSIGNMENT10: Make sure to change invoice bill_to argument to an instance of a Customer class 
+
+customer = Customer('firstname','lastname','512-785-7112')
+
+
+
+
+invoice = Invoice(customer)
+
+keep_going = 'y'
+
+while keep_going == 'y':
+
+    
+
+    #ASSIGNMENT10: Create a product object and add description and cost as parameter arguments.
+    #Quantity parameter remains same.
+
+    
+
+
+
+    invoice.add_invoice_item(InvoiceItem(product, quantity)
+
+    keep_going = input("Enter another type y: ")
+
+
+invoice.print_invoice()
+
+
+if __name__ == "__main__":
+    unittest.main()
+

56a35fd7957da642823bb2c247b6abed655c9125
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-04-18 09:48:28 -0500

    Add files via upload

---
+++ src/homework/homework11/die8.py
@@ -0,0 +1,11 @@
+#HOMEWORK12: add import statement for Die class
+'''
+Define a Die6 class that inherits from the Die class.
+Create a constructor with only one parameter self.
+In the constructor, call the Die class constructor(__init__ method) and pass it parameter arguments self and 8.
+Define a class method named roll.
+In the method:
+ a)display 'Rolled 8 sided die' (MAKE SURE TO USE THE SIDES CLASS ATTRIBUTE; DON'T USE THE NUMBER 8).
+ b)call and return the Die class roll method(This will return the roll value).
+
+'''

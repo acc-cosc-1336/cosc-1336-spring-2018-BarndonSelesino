b0a2b6086802bc75938c981eaa8231e3049e13b4
BarndonSelesino <35587134+BarndonSelesino@users.noreply.github.com>
2018-05-06 22:22:26 -0500

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,4 +1,4 @@
-import src.homework.hw10.main(hw10)
+import src.homework.Newhomework7.main_homework7
 
 #running this file will run the code in main_homework2.py file without the need to modify
 #code in that file

1b000a21d721eca0effb6f162659537a78760c0c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 19:51:44 -0500

    blackboard submission

---
+++ src/final_exam/q_pie/pie_chart.py
@@ -0,0 +1,7 @@
+class Pie_Chart:
+    def __init__(self, list_pie_arcs)
+        self.list_pie_arcs = list_pie_arcs
+
+    def draw(self):
+        Pie_Chart.canvas = tkinter.Canvas(self.main_window, width=200, height=400)
+        Pie_chart.canvas.create_text(100,100, text = text)

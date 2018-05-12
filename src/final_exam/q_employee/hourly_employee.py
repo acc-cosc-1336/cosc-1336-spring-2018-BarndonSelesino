ef272d87021e7b5fe4067b61347d135ee142994a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 20:34:09 -0500

    Final Grading
    -1 inherit from Employee
    -1 constructor requires employee_id and name
    -1 Employee constructor call doesn't require hourly_rate and worked_hours
    -1 line 9, worked_hours is a class attribute access with self.worked_hours

--- src/final_exam/q_employee/hourly_employee.py
+++ src/final_exam/q_employee/hourly_employee.py
@@ -1,9 +1,11 @@
-from employee import Employee
+from src.final_exam.q_employee.employee import Employee
 
-class HourlyEmployee():
-    def __init__(self, hourly_rate, worked_hours):
+class HourlyEmployee(Employee):
+    def __init__(self, employee_id, name, hourly_rate, worked_hours):
+        Employee.__init__(self,employee_id,name)
         self.hourly_rate = hourly_rate
         self.worked_hours = worked_hours
-        Employee.__init__(self,employee_id,name,hourly_rate,worked_hours)
+
     def calculate(self):
-        return self.hourly_rate * worked_hours
+        return self.hourly_rate * self.worked_hours
+

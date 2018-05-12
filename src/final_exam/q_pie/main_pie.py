62ef853ad9261eaec6172336df9e3b851eab81dd
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 20:05:43 -0500

    Final Grading
    -5 Main script incorrect

--- src/final_exam/q_pie/main_pie.py
+++ src/final_exam/q_pie/main_pie.py
@@ -1,9 +1,8 @@
-class Main:
-    def main(self, list_pie_arcs2)
-        self.list_pie_arcs_2 = list_pie_arcs_2
+from src.final_exam.q_pie.pie_arc import PieArc
+from src.final_exam.q_pie.pie_chart import PieChart
 
-        Pie_chart.pie_arc.create_canvas(Pie_chart.main_window, width = 500, height = 500)
-        Pie_Chart.pie_arcs.crate_text(100,100, text = 'one')
-        Pie_Chart.pie_arcs.crate_text(300,100, text = 'two')
-        Pie_Chart.pie_arcs.crate_text(500,100, text = 'three')
-        Pie_Chart.pie_arcs.crate_text(700,100, text = 'four')
+arcs = [PieArc('one'), PieArc('two'), PieArc('three'), PieArc('four')]
+
+pie = PieChart(arcs)
+
+pie.draw()

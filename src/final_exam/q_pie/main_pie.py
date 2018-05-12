c2ee6a9914383716347765f148d2d15b9e3211b7
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 19:52:26 -0500

    blackboard submission

---
+++ src/final_exam/q_pie/main_pie.py
@@ -0,0 +1,9 @@
+class Main:
+    def main(self, list_pie_arcs2)
+        self.list_pie_arcs_2 = list_pie_arcs_2
+
+        Pie_chart.pie_arc.create_canvas(Pie_chart.main_window, width = 500, height = 500)
+        Pie_Chart.pie_arcs.crate_text(100,100, text = 'one')
+        Pie_Chart.pie_arcs.crate_text(300,100, text = 'two')
+        Pie_Chart.pie_arcs.crate_text(500,100, text = 'three')
+        Pie_Chart.pie_arcs.crate_text(700,100, text = 'four')
